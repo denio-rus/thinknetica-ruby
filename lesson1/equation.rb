40a16d3d0f676502ea9258047d31ec0c81c3eb10
denio-rus <denio@yandex.ru>
2018-06-01 23:44:00 +0700

    внесены изменения и переделана индентация

--- lesson1/equation.rb
+++ lesson1/equation.rb
@@ -11,12 +11,12 @@ discriminant = b**2 - 4 * a * c
 
 if discriminant > 0
   discriminant_sqrt = Math.sqrt(discriminant)
-	x1 = (-b + discriminant_sqrt)/(2 * a)
-	x2 = (-b - discriminant_sqrt)/(2 * a)
-	puts "Дискриминант (D = #{discriminant}) больше нуля, значит уравнение имеет два корня: х1= #{x1}  и х2 = #{x2}"
+  x1 = (-b + discriminant_sqrt)/(2 * a)
+  x2 = (-b - discriminant_sqrt)/(2 * a)
+  puts "Дискриминант (D = #{discriminant}) больше нуля, значит уравнение имеет два корня: х1= #{x1}  и х2 = #{x2}"
 elsif discriminant == 0
   x = (-b)/(2 * a)
-	puts "Дискриминант равен нулю, значит уравнение имеет один корень: #{x}"
+  puts "Дискриминант равен нулю, значит уравнение имеет один корень: #{x}"
 else 
-	puts "Дискриминант (D= #{discriminant}) меньше нуля, значит уравнение не имеет действительных корней"
+  puts "Дискриминант (D= #{discriminant}) меньше нуля, значит уравнение не имеет действительных корней"
 end		
