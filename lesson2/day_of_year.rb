17917b1e4ae716dd387d88dd25b4e398eed45795
denio-rus <denio@yandex.ru>
2018-06-09 00:53:15 +0700

    последние правки по первым урокам

--- lesson2/day_of_year.rb
+++ lesson2/day_of_year.rb
@@ -15,11 +15,8 @@ end
 full_year = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 full_year[1] = 29 if year % 4 == 0 && year % 100 != 0 || year % 400 == 0
 
-full_month = 0
 day_of_year = 0
 
-full_month = full_year.first(month-1)
-
-full_month.each { |days_in_month| day_of_year += days_in_month }
+full_year.first(month - 1).each { |days_in_month| day_of_year += days_in_month }
 
 day_of_year += day
